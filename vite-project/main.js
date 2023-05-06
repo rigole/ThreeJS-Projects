facca2f257a3cb787ece36f635be24b18e4edf37
rigole <foplacide@gmail.com>
2023-05-06 13:37:37 +0100

    making the scene with Dat.GUI

--- vite-project/main.js
+++ vite-project/main.js
@@ -3,7 +3,45 @@ import javascriptLogo from './javascript.svg'
 import { setupCounter } from './counter.js'
 
 import * as THREE from 'https://unpkg.com/three@0.146.0/build/three.module.js';
+import * as dat from 'dat.gui';
 
+const gui = new dat.GUI()
+
+const world = {
+    plane: {
+        width: 10,
+        height: 10,
+        widthSegments: 10,
+        heightSegments: 10
+    }
+}
+
+gui.add(world.plane,'width', 1, 20).onChange(generatePlane)
+
+gui.add(world.plane,'height', 1, 20).onChange(generatePlane)
+
+gui.add(world.plane, 'widthSegments', 1, 50).onChange(generatePlane)
+
+gui.add(world.plane, 'heightSegments', 1, 50).onChange(generatePlane)
+
+function generatePlane () {
+    planeMesh.geometry.dispose()
+    planeMesh.geometry = new THREE.PlaneGeometry(
+        world.plane.width,
+        world.plane.height,
+        world.plane.widthSegments,
+        world.plane.heightSegments
+    )
+    
+    const { array } = planeMesh.geometry.attributes.position;
+
+    for (let i = 0; i < array.length; i += 3){
+        const x = array[i]
+        const y = array[i + 1]
+        const z = array[i + 2]
+        array[i + 2] = z + Math.random()
+    }
+}
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, innerWidth / innerHeight, 0.1, 1000)
 
@@ -58,17 +96,6 @@ const light = new THREE.DirectionalLight(0xffffff, 1)
 
 light.position.set(0, 0, 1)
 
-
-const { array } = planeMesh.geometry.attributes.position;
-
-
-for (let i = 0; i < array.length; i += 3){
-    const x = array[i]
-    const y = array[i + 1]
-    const z = array[i + 2]
-
-    array[i + 2] = z + Math.random()
-}
 
 
 
